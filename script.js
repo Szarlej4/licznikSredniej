bcc055e8727d1e50b6240db20269b0209d201149
kuba <jakub.szarleja@gmail.com>
2022-06-15 22:04:17 +0200

    Zmodyfikowanie funkcjonalnosci obliczania sredniej

--- script.js
+++ script.js
@@ -2,59 +2,114 @@ const calculateButton = document.querySelector('#calculateButton');
 const addGradeButton = document.querySelector('#addGradeButton');
 const grades = document.querySelector('#grades');
 const allInputs = document.querySelectorAll('input');
-const averageInfo = document.querySelector('#average')
-let gradesAmount = 2;
+const averageInfo = document.querySelector('#average');
+const allWeightInputs = document.querySelectorAll('.chooseWeightInput');
+const allGradeInputs = document.querySelectorAll('.gradeInput');
+let sum = 0;
 let average = 0;
+
+for (let input of allGradeInputs) {
+    input.addEventListener('click', () => {
+    })
+}
+
+for (let input of allWeightInputs) {
+    const allGradeInputs = document.querySelectorAll(`#gradesWeight${input.value} .gradesInput`);
+    const gradeInformation = document.querySelector(`#gradesInformation${input.value}`);
+    for (let gradeInput of allGradeInputs) {
+        gradeInput.addEventListener('click', () => {
+            gradeInformation.innerHTML += `${gradeInput.value} `;
+            // sum += gradeInput.value * input.value;
+            // average += input.value * 1;
+            // console.log(sum);
+            // console.log(average);
+            // console.log(Math.round((sum / average) * 1000) / 1000);
+        })
+    }
+    input.addEventListener('change', () => {
+        let elementId = document.querySelector(`#gradesWeight${input.value}`);
+        elementId.classList.toggle('hidden');
+    })
+}
 
 calculateButton.addEventListener('click', () => {
     let average = calculateAverage();
     averageInfo.innerHTML = `Twoja srednia to: ${average}`;
 })
 
-addGradeButton.addEventListener('click', () => {
-    createGradeContainer();
-})
+// const createElements = (weight, checked) => {
+//     for (let input of allWeightInputs) {
+//         if (input.checked) {
+//             if (checked && document.querySelector(`#weight${weight}Grades`) === null) {
+//                 const createGradesContainer = document.createElement('div');
+//                 const createInputGradeTitle = document.createElement('h2');
+//                 const createSpecifyGradeBox = document.createElement('div');
 
-const createGradeContainer = () => {
-    const createGrade = document.createElement('div');
-    const createInputGradeTitle = document.createElement('h2');
-    const createSpecifyGradeBox = document.createElement('div');
-    const createLabelForGrade = document.createElement('label');
-    const createGradeInput = document.createElement('input');
-    const createLabelForWeight = document.createElement('label');
-    const createWeightInput = document.createElement('input');
+//                 createGradesContainer.className = 'grade';
+//                 createGradesContainer.id = `weight${weight}Grades`
+//                 grades.append(createGradesContainer);
 
-    gradesAmount++;
+//                 createInputGradeTitle.innerHTML = `oceny waga ${weight}`;
+//                 createGradesContainer.append(createInputGradeTitle);
 
-    createGrade.className = 'grade';
-    grades.append(createGrade);
+//                 createSpecifyGradeBox.className = 'specifyGradeBox';
+//                 createGradesContainer.append(createSpecifyGradeBox);
 
-    createInputGradeTitle.innerHTML = 'Wpisz ocene';
-    createGrade.append(createInputGradeTitle);
+//                 for (let i = 1; i < 7; i++) {
+//                     const createGradeInputButton = document.createElement('button');
+//                     createGradeInputButton.id = `${weight}${i}`;
+//                     createGradeInputButton.innerHTML = i;
+//                     createSpecifyGradeBox.append(createGradeInputButton);
+//                 }
+//             }
+//         }
+//     }
+// }
 
-    createSpecifyGradeBox.className = 'specifyGradeBox';
-    createGrade.append(createSpecifyGradeBox);
+// addGradeButton.addEventListener('click', () => {
+//     createGradeContainer();
+// })
 
-    createLabelForGrade.setAttribute('for', `grade${gradesAmount}`);
-    createLabelForGrade.innerHTML = "Ocena";
+// const createGradeContainer = () => {
+//     const createGrade = document.createElement('div');
+//     const createInputGradeTitle = document.createElement('h2');
+//     const createSpecifyGradeBox = document.createElement('div');
+//     const createLabelForGrade = document.createElement('label');
+//     const createGradeInput = document.createElement('input');
+//     const createLabelForWeight = document.createElement('label');
+//     const createWeightInput = document.createElement('input');
 
-    setInputAttributes(createGradeInput, "6", "grade");
-    createGradeInput.className = "gradeInput";
+//     gradesAmount++;
 
-    createLabelForWeight.setAttribute('for', `weight${gradesAmount}`);
-    createLabelForWeight.innerHTML = "Ocena";
+//     createGrade.className = 'grade';
+//     grades.append(createGrade);
 
-    setInputAttributes(createWeightInput, "4", "weight");
-    createWeightInput.className = "weightInput";
+//     createInputGradeTitle.innerHTML = 'Wpisz ocene';
+//     createGrade.append(createInputGradeTitle);
 
-    createSpecifyGradeBox.append(createLabelForGrade, createGradeInput, createLabelForWeight, createWeightInput);
-}
+//     createSpecifyGradeBox.className = 'specifyGradeBox';
+//     createGrade.append(createSpecifyGradeBox);
 
-const setInputAttributes = (element, value, input) => {
+//     createLabelForGrade.setAttribute('for', `grade${gradesAmount}`);
+//     createLabelForGrade.innerHTML = "Ocena";
+
+//     setInputAttributes(createGradeInput, "6", "grade");
+//     createGradeInput.className = "gradeInput";
+
+//     createLabelForWeight.setAttribute('for', `weight${gradesAmount}`);
+//     createLabelForWeight.innerHTML = "Ocena";
+
+//     setInputAttributes(createWeightInput, "4", "weight");
+//     createWeightInput.className = "weightInput";
+
+//     createSpecifyGradeBox.append(createLabelForGrade, createGradeInput, createLabelForWeight, createWeightInput);
+// }
+
+const setInputAttributes = (element, id) => {
     element.setAttribute('type', 'number');
     element.setAttribute('min', '1');
-    element.setAttribute('max', `${value}`);
-    element.setAttribute('id', `${input}${gradesAmount}`);
+    element.setAttribute('max', `6`);
+    element.setAttribute('id', `${id}`);
 }
 
 const calculateAverage = () => {
