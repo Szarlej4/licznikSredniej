6d842c1fa2db169cc3ad5c2ce3e84704164567ea
kuba <jakub.szarleja@gmail.com>
2022-06-17 23:58:11 +0200

    working on local storage usage in app to store data

--- script.js
+++ script.js
@@ -1,152 +1,140 @@
-const calculateButton = document.querySelector('#calculateButton');
-const addGradeButton = document.querySelector('#addGradeButton');
-const grades = document.querySelector('#grades');
-const allInputs = document.querySelectorAll('input');
-const averageInfo = document.querySelector('#average');
-const allWeightInputs = document.querySelectorAll('.chooseWeightInput');
-const allGradeInputs = document.querySelectorAll('.gradeInput');
-let sum = 0;
-let average = 0;
-let weightSum = 0;
+const grades = document.querySelector('#grades'); // grades selector
+const averageInfo = document.querySelector('#average'); // average info selector
+const allWeightInputs = document.querySelectorAll('.chooseWeightInput'); // all weight inputs selector
+localStorage.setItem('sum', '0'); // sum for counting average declared in global scope 
+let sum = parseFloat(localStorage.getItem('sum'));
+localStorage.setItem('average', '0');
+average = parseFloat(localStorage.getItem('average'));
+localStorage.setItem(`weightSum`, '0');
+let weightSum = parseFloat(localStorage.getItem(`weightSum`));
 
-for (let input of allGradeInputs) {
-    input.addEventListener('click', () => {
-    })
-}
+const createGradesInput = (i) => {
+    localStorage.setItem(`gradesOfWeight${i}`, '0');
+    localStorage.setItem(`weight${i}Sum`, '0');
+    let weightSum = parseFloat(localStorage.getItem(`weight${i}Sum`));
+    let gradesOfThisWeight = parseFloat(localStorage.getItem(`gradesOfWeight${i}`));
+    const createGrade = document.createElement('div');
+    const createGradesBoxTitle = document.createElement('h2');
+    const createSpecifyGradesBox = document.createElement('div');
+    const createActualGradesInformation = document.createElement('p');
 
-for (let input of allWeightInputs) {
-    const allGradeInputs = document.querySelectorAll(`#gradesWeight${input.value} .gradesInput`);
-    const gradeInformation = document.querySelector(`#gradesInformation${input.value}`);
+    createGrade.classList.add('grade');
+    createGrade.id = `gradesWeight${i}`;
 
-    for (let gradeInput of allGradeInputs) {
-        gradeInput.addEventListener('click', () => {
-            const createGrade = document.createElement('span');
+    createGradesBoxTitle.innerHTML = `Wybierz oceny waga ${i}`;
+
+    createSpecifyGradesBox.classList.add('specifyGradeBox');
+
+    createActualGradesInformation.classList.add('actualGradeInformation');
+    createActualGradesInformation.id = `gradesInformation${i}`;
+
+    grades.append(createGrade);
+    createGrade.append(createGradesBoxTitle, createSpecifyGradesBox, createActualGradesInformation);
+    for (let j = 1; j < 7; j++) {
+        localStorage.setItem(`weightSum`, '0');
+        let weightSum = parseFloat(localStorage.getItem(`weightSum`));
+        const createGradesInput = document.createElement('button');
+        createGradesInput.classList.add('gradesInput');
+        createGradesInput.value = j;
+        createGradesInput.innerHTML = j;
+        createSpecifyGradesBox.append(createGradesInput);
+
+        createGradesInput.addEventListener('click', () => {
+            const createGradeItem = document.createElement('span');
             const createTooltip = document.createElement('span');
 
-            createTooltip.innerHTML = "Wcisnij aby usunac srednia";
+            createTooltip.innerHTML = "Wcisnij aby usunac ocene";
             createTooltip.classList.toggle('gradeTooltip');
-            createGrade.innerHTML = `${gradeInput.value} `;
-            createGrade.classList.toggle('createdGrade');
-            gradeInformation.append(createGrade);
-            createGrade.append(createTooltip);
+            createGradeItem.innerHTML = `${j} `;
+            createGradeItem.classList.toggle('createdGrade');
+            createActualGradesInformation.append(createGradeItem);
+            createGradeItem.append(createTooltip);
 
-            createGrade.addEventListener('click', () => {
-                sum -= gradeInput.value * input.value;
-                weightSum -= input.value * 1;
-                let average = Math.round((sum / weightSum) * 1000) / 1000;
-                averageInfo.innerHTML = Math.round((sum / weightSum) * 1000) / 1000;
+            sum += createGradesInput.value * i;
+            console.log(`sum: ${sum}`);
+            localStorage.setItem('sum', `${sum}`);
+            let kkk = localStorage.getItem('sum')
+            console.log(`localStorage sum: ${kkk}`);
+            weightSum += i * 1;
+            console.log(`weightSum: ${weightSum}`);
+            localStorage.setItem(`weight${i}Sum`, `${weightSum}`);
+            let lll = localStorage.getItem(`weight${i}Sum`);
+            console.log(`localStorage weightSum: ${lll}`);
+            average = Math.round((sum / weightSum) * 1000) / 1000;
+            averageInfo.innerHTML = average;
+            console.log(`average: ${average}`);
+            localStorage.setItem('average', `${average}`);
+            let jjj = localStorage.getItem('average');
+            console.log(`localStorage average: ${jjj}`);
+            let thisGradeLocalStorageId = `weight${i}grade${gradesOfThisWeight}`;
+            console.log(`this Grade Local Storage Id: ${thisGradeLocalStorageId}`);
+            localStorage.setItem(thisGradeLocalStorageId, `${createGradesInput.value}`);
+            console.log(`this grade local storage id value${createGradesInput.value}`);
+
+
+            createGradeItem.addEventListener('click', () => {
+                sum -= createGradesInput.value * i;
+                console.log(`sum: ${sum}`);
+                localStorage.setItem('sum', `${sum}`);
+                let ppp = localStorage.getItem('sum');
+                console.log(`localStorage sum: ${ppp}`);
+                weightSum -= i * 1;
+                console.log(`weightSum: ${weightSum}`);
+                localStorage.setItem(`weight${i}Sum`, `${weightSum}`);
+                let sss = localStorage.getItem(`weight${i}Sum`);
+                console.log(`localStorage weight sum: ${sss}`);
+                average = Math.round((sum / weightSum) * 1000) / 1000;
+                console.log(`average: ${average}`);
+                localStorage.setItem('average', `${average}`);
+                let fff = localStorage.getItem(`average`);
+                console.log(`localStorage average: ${fff}`);
+                averageInfo.innerHTML = average;
                 if (isNaN(average)) {
                     averageInfo.innerHTML = '???';
+                    localStorage.setItem('average', '0');
                 }
-                createGrade.classList.toggle('hidden');
+                localStorage.removeItem(thisGradeLocalStorageId);
+                createGradeItem.remove();
             })
 
-            sum += gradeInput.value * input.value;
-            weightSum += input.value * 1;
-            averageInfo.innerHTML = Math.round((sum / weightSum) * 1000) / 1000;
-            // sum += gradeInput.value * input.value;
-            // average += input.value * 1;
-            // console.log(sum);
-            // console.log(average);
-            // console.log(Math.round((sum / average) * 1000) / 1000);
+            gradesOfThisWeight++;
+            localStorage.setItem(`gradesOfWeight${i}`, `${gradesOfThisWeight}`);
         })
     }
-    input.addEventListener('change', () => {
-        let elementId = document.querySelector(`#gradesWeight${input.value}`);
-        elementId.classList.toggle('hidden');
-    })
 }
 
-calculateButton.addEventListener('click', () => {
-    let average = calculateAverage();
-    averageInfo.innerHTML = `Twoja srednia to: ${average}`;
-})
-
-// const createElements = (weight, checked) => {
-//     for (let input of allWeightInputs) {
-//         if (input.checked) {
-//             if (checked && document.querySelector(`#weight${weight}Grades`) === null) {
-//                 const createGradesContainer = document.createElement('div');
-//                 const createInputGradeTitle = document.createElement('h2');
-//                 const createSpecifyGradeBox = document.createElement('div');
-
-//                 createGradesContainer.className = 'grade';
-//                 createGradesContainer.id = `weight${weight}Grades`
-//                 grades.append(createGradesContainer);
-
-//                 createInputGradeTitle.innerHTML = `oceny waga ${weight}`;
-//                 createGradesContainer.append(createInputGradeTitle);
-
-//                 createSpecifyGradeBox.className = 'specifyGradeBox';
-//                 createGradesContainer.append(createSpecifyGradeBox);
-
-//                 for (let i = 1; i < 7; i++) {
-//                     const createGradeInputButton = document.createElement('button');
-//                     createGradeInputButton.id = `${weight}${i}`;
-//                     createGradeInputButton.innerHTML = i;
-//                     createSpecifyGradeBox.append(createGradeInputButton);
-//                 }
-//             }
-//         }
-//     }
-// }
-
-// addGradeButton.addEventListener('click', () => {
-//     createGradeContainer();
-// })
-
-// const createGradeContainer = () => {
-//     const createGrade = document.createElement('div');
-//     const createInputGradeTitle = document.createElement('h2');
-//     const createSpecifyGradeBox = document.createElement('div');
-//     const createLabelForGrade = document.createElement('label');
-//     const createGradeInput = document.createElement('input');
-//     const createLabelForWeight = document.createElement('label');
-//     const createWeightInput = document.createElement('input');
-
-//     gradesAmount++;
-
-//     createGrade.className = 'grade';
-//     grades.append(createGrade);
-
-//     createInputGradeTitle.innerHTML = 'Wpisz ocene';
-//     createGrade.append(createInputGradeTitle);
-
-//     createSpecifyGradeBox.className = 'specifyGradeBox';
-//     createGrade.append(createSpecifyGradeBox);
-
-//     createLabelForGrade.setAttribute('for', `grade${gradesAmount}`);
-//     createLabelForGrade.innerHTML = "Ocena";
-
-//     setInputAttributes(createGradeInput, "6", "grade");
-//     createGradeInput.className = "gradeInput";
-
-//     createLabelForWeight.setAttribute('for', `weight${gradesAmount}`);
-//     createLabelForWeight.innerHTML = "Ocena";
-
-//     setInputAttributes(createWeightInput, "4", "weight");
-//     createWeightInput.className = "weightInput";
-
-//     createSpecifyGradeBox.append(createLabelForGrade, createGradeInput, createLabelForWeight, createWeightInput);
-// }
-
-const setInputAttributes = (element, id) => {
-    element.setAttribute('type', 'number');
-    element.setAttribute('min', '1');
-    element.setAttribute('max', `6`);
-    element.setAttribute('id', `${id}`);
+const removeGradesInput = (gradesInput) => {
+    let boxInputToRemove = document.querySelector(`#gradesWeight${gradesInput}`);
+    boxInputToRemove.remove();
 }
 
-const calculateAverage = () => {
-    let gradeSum = 0;
-    let weightSum = 0;
-    for (let i = 1; i <= gradesAmount; i++) {
-        let grade = document.querySelector(`#grade${i}`).valueAsNumber;
-        let weight = document.querySelector(`#weight${i}`).valueAsNumber;
-        for (let j = 0; j < weight; j++) {
-            gradeSum += grade;
+for (let i = 1; i < 11; i++) {
+    let weightInput = document.querySelector(`#inputWeight${i}`);
+    let isWeightChecked = document.querySelector(`#inputWeight${i}`).checked;
+    if (isWeightChecked || localStorage.getItem(`#inputWeight${i}`) !== "0") {
+        localStorage.setItem(`#inputWeight${i}`, '1');
+        document.querySelector(`#inputWeight${i}`).checked = true;
+        if (localStorage.getItem(`isWeight${i}Created`) === '1') {
+            createGradesInput(i);
+            console.log('created already');
+        } else {
+            createGradesInput(i);
+            localStorage.setItem(`isWeight${i}Created`, '1');
+            console.log('it was never created');
         }
-        weightSum += weight;
+    } else {
+        localStorage.setItem(`#inputWeight${i}`, '0');
     }
-    return Math.round((gradeSum / weightSum) * 1000) / 1000;
+    weightInput.addEventListener('click', () => {
+        let isWeightChecked = document.querySelector(`#inputWeight${i}`).checked;
+        if (!isWeightChecked) {
+            localStorage.setItem(`#inputWeight${i}`, '0');
+            removeGradesInput(i);
+            console.log("is not checked");
+        } else {
+            localStorage.setItem(`#inputWeight${i}`, '1');
+            createGradesInput(i);
+            console.log("ischecked");
+        }
+    })
 }
